fb4c1e50e1e680b643014a523bf89e79153a223f
Alina <kolmykova93@gmail.com>
2020-09-21 11:25:28 +0200

    Create project

---
+++ Mrs_Cake/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mrs_Cake.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
