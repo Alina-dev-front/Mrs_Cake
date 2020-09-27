755eedc2f97669f3df0eec5358fd5d9cd132eea0
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-09-27 12:47:07 +0200

    Routing to bakeryRegistration &  HomePage created

--- mrscake/src/App.js
+++ mrscake/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {HomePage} from './components/HomePage';
+import {BakeryRegistration} from './components/BakeryRegistration';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div>Hello
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={HomePage} exact/>
+        <Route path='/bakeryregistration' component={BakeryRegistration}/>
+      </Switch>
+    </BrowserRouter>
+
   );
 }
-
 export default App;
