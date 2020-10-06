86d8e5924289429fdc1cf01ff42cc2fb4cb858c9
Parul76 <66684404+Parul76@users.noreply.github.com>
2020-10-06 22:07:23 +0200

    Parul login

---
+++ Mrs_Cake/LoginDetails/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mrs_Cake.LoginDetails
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+      
+    }
+}
