39c41a510c3e531ef05fec0c28bfc9c142bf06bf
Alina-dev-front <55713259+Alina-dev-front@users.noreply.github.com>
2020-10-12 14:51:08 +0200

    Update ProductsControllers.cs

--- Mrs_Cake/Controllers/ProductsControllers.cs
+++ Mrs_Cake/Controllers/ProductsControllers.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Mrs_Cake.Models;
 using Mrs_Cake.Services;
@@ -10,22 +10,14 @@ namespace Mrs_Cake.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-<<<<<<< HEAD
         private readonly ProductService _productService;
 
         public ProductsController(ProductService productService)
         {
             _productService = productService;
-=======
-        private readonly DisconnectedRepository _repo ;
-        public ProductsController(DisconnectedRepository repository)
-        {
-            this._repo = repository;
->>>>>>> master
         }
 
         [HttpGet]
-<<<<<<< HEAD
         public ActionResult<List<Product>> Get() {
             List<Product> productsFromDB = _productService.Get();
             if (productsFromDB == null)
@@ -33,18 +25,12 @@ namespace Mrs_Cake.Controllers
                 return NotFound();
             }
             return productsFromDB;
-=======
-        public IEnumerable<Product> Get()
-        {
-            return _repo.GetProducts();
->>>>>>> master
         }
 
 
         [HttpGet("{id:length(24)}", Name = "GetProduct")]
         public ActionResult<Product> GetById(string id)
         {
-<<<<<<< HEAD
             var productFromDB = _productService.GetById(id);
 
             if (productFromDB == null)
@@ -52,15 +38,11 @@ namespace Mrs_Cake.Controllers
                 return NotFound();
             }
             return productFromDB;
-=======
-            return Ok(_repo.GetProductById(id));
->>>>>>> master
         }
 
         [HttpPost]
         public ActionResult<Product> Create(Product product)
         {
-<<<<<<< HEAD
             _productService.Create(product);
 
             return CreatedAtRoute("GetProduct", new { id = product.Id.ToString() }, product);
@@ -91,21 +73,8 @@ namespace Mrs_Cake.Controllers
             }
             _productService.Remove(productFromDB.Id);
 
-=======
-            return CreatedAtAction("Get", new { id = product.Id }, _repo.SaveNewProduct(product));
-        }
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put([FromBody] Product product)
-        {
-            _repo.SaveUpdatedProduct(product);
             return NoContent();
         }
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(long id)
-        {
-            _repo.DeleteProduct(id);
->>>>>>> master
-            return NoContent();
-        }
+        
     }
 }
