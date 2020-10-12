8be3758493b0414238aa3ede9c5cfc87fedcef69
Alina-dev-front <55713259+Alina-dev-front@users.noreply.github.com>
2020-10-12 14:41:22 +0200

    Update Product.cs

--- Mrs_Cake/Models/Product.cs
+++ Mrs_Cake/Models/Product.cs
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Mrs_Cake.Interfaces;
->>>>>>> master
 
 namespace Mrs_Cake.Models 
 {
-    public class Product : ImodifactionHistory
+    public class Product
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
@@ -22,9 +19,5 @@ namespace Mrs_Cake.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-
-        public DateTime DateModified { get; set; }
-        public DateTime DateCreated { get; set; }
-        public bool IsDirty { get; set; }
     }
 }
