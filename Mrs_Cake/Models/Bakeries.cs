7ee2d545ecefdfd620c9fd4cdac672539ca0c816
Alina <kolmykova93@gmail.com>
2020-10-12 15:02:28 +0200

    correct errors

--- Mrs_Cake/Models/Bakeries.cs
+++ Mrs_Cake/Models/Bakeries.cs
@@ -2,20 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Mrs_Cake.Interfaces;
 
 namespace Mrs_Cake.Models
 {
-    public class Bakeries : ImodifactionHistory
+    public class Bakeries
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-
-        public DateTime DateModified { get; set; }
-        public DateTime DateCreated { get; set; }
-        public bool IsDirty { get; set; }
     }
 }
