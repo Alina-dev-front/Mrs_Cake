66d6047df42212ec6786c8728c647c041a126fe3
Alina <kolmykova93@gmail.com>
2020-10-19 13:48:06 +0200

    edit routin with Link

--- mrscake/src/App.js
+++ mrscake/src/App.js
@@ -14,43 +14,26 @@ import NavBar from './components/NavBar';
 import FooterPage from './components/Footer';
 import CarouselControlled from "./components/Carrousel";
 import './App.css';
+
 function App() {
   return (
     <React.Fragment>
-    <NavBar />
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' component={HomePage} exact />
-        <Route path="/aboutus" component={AboutUs} />
-        <Route path="/contactus" component={ContactUs} />
-        <Route path="/shoppingcart" component={shoppingCart} />
-        <Route path="/productpage" component={ProductTable} />
-        <Route path="/homepage" component={CarouselControlled}/>
-        <div className="container">    
-        <nav classN-ame="navbar navbar-expand-lg navheader">    
-          <div className="collapse navbar-collapse" >    
-            <ul className="navbar-nav mr-auto">    
-              <li className="nav-item">    
-              <a class="nav-link" href="Login">Login</a>   
-              </li>    
-              <li className="nav-item">    
-              <a class="nav-link" href="Signup">SignUp</a> 
-              </li>      
-            </ul>    
-          </div>
-        </nav> <br />  
-        </div>
-     </Switch> 
-     <Switch>    
-          <Route  path='/Login' component={Login} />    
-          <Route path='/SignUp' component={SignUp} />    
-          <Route  path='/ForgotPassword' component={ForgotPassword}/>
-     </Switch>    
-     <Switch>  
-        <Route path='/Dashboard' component={Dashboard} />    
-     </Switch>  
-    </BrowserRouter>
-    <FooterPage />
+        <BrowserRouter>
+        <NavBar />
+          <Switch>
+            <Route path='/' component={HomePage} exact />
+            <Route path="/aboutus" component={AboutUs} />
+            <Route path="/contactus" component={ContactUs} />
+            <Route path="/shoppingcart" component={shoppingCart} />
+            <Route path="/productpage" component={ProductTable} />
+            <Route path="/homepage" component={CarouselControlled}/>
+            <Route  path='/Login' component={Login} />    
+            <Route path='/SignUp' component={SignUp} />    
+            <Route  path='/ForgotPassword' component={ForgotPassword}/>
+            <Route path='/Dashboard' component={Dashboard} />  
+          </Switch> 
+        </BrowserRouter>
+      <FooterPage />
     </React.Fragment>
     );
   }
