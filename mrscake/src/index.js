c7e4ee5ad55f1a5c856f613d069555c3801aca46
Abdelilah <abdiachir@gmail.com>
2020-10-20 10:44:03 +0200

    "added reducers to the shopping cart"

--- mrscake/src/index.js
+++ mrscake/src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createStore } from 'redux';
+import rootReducer from './reducers';
+import {Provider} from 'react-redux';
+
+
+
+export const  store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
