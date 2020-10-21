24338b64244177d227ddb5e6b396811f4df273e9
Alina-dev-front <55713259+Alina-dev-front@users.noreply.github.com>
2020-10-21 11:40:05 +0200

    Update index.js

--- mrscake/src/index.js
+++ mrscake/src/index.js
@@ -8,10 +8,7 @@ import {createStore } from 'redux';
 import rootReducer from './reducers';
 import {Provider} from 'react-redux';
 
-
-
 export const  store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 
 ReactDOM.render(
   <Provider store={store}>
