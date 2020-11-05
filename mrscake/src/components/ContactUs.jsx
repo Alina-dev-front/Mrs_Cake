b5588fbb2e2758d5054a1814845bae265f81161b
Parul76 <66684404+Parul76@users.noreply.github.com>
2020-11-05 23:23:22 +0100

    changes in contact us page

--- mrscake/src/components/ContactUs.jsx
+++ mrscake/src/components/ContactUs.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import contactus from '../contactus.jpg';
+
 function ContactUs() {
 
 return <div><br></br>
@@ -7,16 +9,22 @@ return <div><br></br>
       
             <div class="title-wrap space-bottom-20">
             <div class="col-md-6 col-sm-12">
-                <p class="Protit" >
-               
-                 <h4  style={{color: "Red", paddingTop: "100px"}}>Do you need to speak to someone at Mrscake store
-                   directly? Find contact information and customer service details here. </h4> 
-                </p>  
+                
                 </div>
-                <div class="our-history space-bottom-20 space-top-20" style={{paddingTop: "100px"}}>
-                <div class="col-md-6 col-sm-12" style={{paddingTop: "100px"}}>    
-                   <p> <h5>Contact us</h5><br/></p> </div><br/>
-                   <p style={{paddingBottom: "100px"}}> 
+                <div class="our-history space-bottom-20 space-top-20" style={{paddingBottom: "100px"}}>
+                <div class="col-md-6 col-sm-12" style={{paddingBottom: "100px"}}>    
+                   <p> <h5 style={{color: "Red", paddingTop: "100px"}}>Contact us</h5><br/></p> </div><br/>
+                   <p style={{paddingTop: "30px"}}> 
+                   <div className="col-md-3 center" style={{paddingTop: "20px"}}>
+                
+                <img style={{paddingBottom: "80px"}}
+         src={contactus}
+        width="400"
+        height="250"
+        className="d-inline-block align-top"
+        alt="Min cake"
+      />
+      </div>
                     <b>Telephone numbers and opening hours</b><br/>
                     <b>Customer Service</b><br/>
                     <b>Opening hours:</b> Mon–Fri 8.00 a.m. – 7.00 p.m.<br/>
