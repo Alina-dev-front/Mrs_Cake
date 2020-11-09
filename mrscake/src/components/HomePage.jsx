33797710a89d03fc662e19920973766e6fd3772e
Alina <kolmykova93@gmail.com>
2020-11-10 00:38:35 +0100

    home page feature = locations

--- mrscake/src/components/HomePage.jsx
+++ mrscake/src/components/HomePage.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import CarouselControlled from "./Carrousel";
 import FooterPage from '../components/Footer';
+import { Card, Col, Row } from 'react-bootstrap';
+import './HomePage.css';
+
 
 export class HomePage extends Component{
     render(){
         return( <React.Fragment>
                     <CarouselControlled/>
+                    <Card className="location-home-page">
+                        <Card.Header style={{textAlign: "center"}}>Our most popular locations</Card.Header>
+                        <Card.Body>
+                            <Row>
+                            <Col>
+                                <Card.Title>Stockholm</Card.Title>
+                                <Card.Title>Gothenburg</Card.Title>
+                            </Col>
+                            <Col>
+                                <Card.Title>Lund</Card.Title>
+                                <Card.Title>Helsinborg</Card.Title>
+                            </Col>
+                            <Col>
+                                <Card.Title>Falkenberg</Card.Title>
+                                <Card.Title>Visby</Card.Title>
+                            </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
                     <FooterPage />
                 </React.Fragment>
         );
