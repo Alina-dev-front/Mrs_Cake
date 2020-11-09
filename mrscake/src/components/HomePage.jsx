2fcdc76f612bd7b91fa3ba8f0eb9670d0834bce3
Alina <kolmykova93@gmail.com>
2020-11-09 23:35:27 +0100

    fix carousel bug

--- mrscake/src/components/HomePage.jsx
+++ mrscake/src/components/HomePage.jsx
@@ -4,18 +4,10 @@ import FooterPage from '../components/Footer';
 
 export class HomePage extends Component{
     render(){
-        let wrapperStyle = {
-            margin: 'auto',
-            top: '0%',
-            position: 'center',
-            width: '98%',
-            height: '150px',           
-          };
-        return(
-            <div style={wrapperStyle}>
-                <CarouselControlled/>
-                <FooterPage />
-            </div>
+        return( <React.Fragment>
+                    <CarouselControlled/>
+                    <FooterPage />
+                </React.Fragment>
         );
     }
 }
