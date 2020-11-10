232758d1c277cdf949516384688255ec208b0a23
Parul76 <66684404+Parul76@users.noreply.github.com>
2020-11-10 11:29:17 +0100

    bakeryfilq

--- mrscake/src/components/BakeryTable.js
+++ mrscake/src/components/BakeryTable.js
@@ -1,52 +1,30 @@
 import React, { Component } from 'react';
 import { Table } from 'reactstrap';
-import { PRODUCTS_API_URL } from '../constants/api_url_path';
 import {connect} from 'react-redux';
 import "./DataTable.css";
 
 
 class DataTable extends Component {
-  deleteItem = id => {
-    let confirmDeletion = window.confirm('Do you really wish to delete it?');
-    if (confirmDeletion) {
-      fetch(`${PRODUCTS_API_URL}/${id}`, {
-        method: 'delete',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(res => {
-          this.props.deleteItemFromState(id);
-        })
-        .catch(err => console.log(err));
-    }
-  }
- 
+
+
   render() {
     let items = this.props.items;
     if (this.props.filteredItems.length > 0) {
       items = this.props.filteredItems;
-    }
-
-   
-    
-    
+    }    
     return <Table striped>
       <thead className="thead-dark">
         <tr>
-         
           <th className="id-column">Product Id</th>
           <th className="productType-column">Product Type</th>
           <th className="name-column">Name</th>
           <th className="bakery-column">Bakery</th>
           <th className="price-column">Price</th>
-         
         </tr>
       </thead>
       <tbody>
         {!items || items.length <= 0 ?
           <tr>
-            
           </tr>
           : items.map(item => (
             <tr key={item.id}>
