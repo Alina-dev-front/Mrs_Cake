28a3de686f0eca78700edfa7c3494f281bac4f0e
Alina <kolmykova93@gmail.com>
2020-11-16 16:00:32 +0100

    first product test

--- Test_Mrs_Cake/UnitTest1.cs
+++ Test_Mrs_Cake/UnitTest1.cs
@@ -11,13 +11,23 @@ namespace Test_Mrs_Cake
 {
     public class ProductServiceTest
     {
-        
+        private readonly IMongoCollection<Product> _products;
+
+        public ProductServiceTest()
+        {
+            var client = new MongoClient("mongodb+srv://Alina_Iakimchuk:Greenday15@mrscakecluster.vsx9o.azure.mongodb.net/Mrs_Cake?retryWrites=true&w=majority");
+            var database = client.GetDatabase("Mrs_Cake");
+            _products = database.GetCollection<Product>("Products_Test");
+        }
+
         [Fact]
         public void CountProductsInDb()
         {
-            /*List<Product> result = ProductService.Get();
+            List<Product> testList = _products.Find(product => true).ToList();
 
-            Assert.Equal(18, result.Count);*/
+            int numberOfProductsInTestDB = 18;
+
+            Assert.Equal(numberOfProductsInTestDB, testList.Count);
         }
     }
 }
