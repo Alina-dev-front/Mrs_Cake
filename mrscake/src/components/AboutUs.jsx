a3d5250bc832397af8d05ba24aeb04db97ee4113
Alina <kolmykova93@gmail.com>
2020-11-27 17:12:07 +0100

    add space

--- mrscake/src/components/AboutUs.jsx
+++ mrscake/src/components/AboutUs.jsx
@@ -16,7 +16,7 @@ function AboutUs() {
 
                 <p style={{fontSize: "100%"}} >
                 <b> MrsCake</b> shop is a famous brand that started as a small business. 
-                The owners are <b>Parul</b>, <b>Alina</b> and <b>Abel</b>and MrsCake supported by owner and staff.<br /><br />
+                The owners are <b>Parul</b>, <b>Alina</b> and <b>Abel</b> and MrsCake supported by owner and staff.<br /><br />
                 Although not small any more,  our products and staff consider each customer 
                 a member in this family of cake shoppers.
                 Our mission is to make people happy.<br /> Keeping our prices reasonable are all different ways to achieve this goal.
